2a13065838c10a1529c2ef44be0d1a1e65f27826
kontrollanten <alexander@width.se>
2018-12-18 19:33:42 +0100

    check-for-updates: spara shape-fil i nuvarande katalog

--- check-for-updates/store-shapefile.go
+++ check-for-updates/store-shapefile.go
@@ -5,7 +5,7 @@ import (
 )
 
 func StoreShapefile(data []byte) string {
-  storagePath := "/go/src/update-shelters/skyddsrum.zip"
+  storagePath := "./skyddsrum.zip"
   err := ioutil.WriteFile(storagePath, data, 0644)
   if err != nil {
     panic(err)
