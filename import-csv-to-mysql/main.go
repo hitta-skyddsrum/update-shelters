be9baa4dc59e0100372b7c7c0919907d76471762
kontrollanten <alexander@width.se>
2019-02-04 00:32:30 +0100

    import-csv-to-mysql: använd samma schema vid CLI som i Lambda

--- import-csv-to-mysql/main.go
+++ import-csv-to-mysql/main.go
@@ -6,12 +6,10 @@ import (
   "database/sql"
   "flag"
   "fmt"
-  "io/ioutil"
   "os"
 )
 
 func main() {
-  sqlMigration := flag.String("sql-migration", "", "SQL file containing migration for shelters table")
   dbU := flag.String("db-user", "", "Database user")
   dbP := flag.String("db-password", "", "Database password")
   dbA := flag.String("db-address", "", "Database address, including port number.")
@@ -31,19 +29,11 @@ func main() {
 
   csvFile := flag.Args()[0]
 
-  b, err := ioutil.ReadFile(*sqlMigration)
-  if err != nil {
-    panic(err)
-  }
-  fmt.Println("SQL migration file open.")
-
-  s := string(b)
-
   db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@(%s)/", *dbU, *dbP, *dbA))
   if err != nil {
     panic(err)
   }
   fmt.Println("Database connection open.")
 
-  ImportCsvToMysql(db, s, csvFile)
+  ImportCsvToMysql(db, schema, csvFile)
 }
