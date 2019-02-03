a31e6ef4cc43def8e500abe8a1e4b1b8b4d9f50c
kontrollanten <alexander@width.se>
2019-02-04 00:38:03 +0100

    import-csv-to-mysql: döp om kolumn-namn i shelters-tabellen
    closes #2

--- import-csv-to-mysql/schema.go
+++ import-csv-to-mysql/schema.go
@@ -2,23 +2,25 @@ package main
 
 const (
 schema = "CREATE TABLE `shelters` (" +
-  "`InspireID` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL," +
-  "`beginLifes` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL," +
-  "`endLifeSpa` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL," +
-  "`typeOfOccu` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL," +
-  "`resourceFi` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL," +
+  "`inspire_id` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL," +
+  "`begin_life` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL," +
+  "`end_life` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL," +
+  "`type_of_occupation` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL," +
+  "`filter_type` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL," +
   "`serviceLev` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL," +
-  "`serviceTyp` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL," +
+  "`service_type` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL," +
   "`pointOfCon` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL," +
   "`relatedPar` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL," +
-  "`numberOfOc` int(11) DEFAULT NULL," +
-  "`additional` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL," +
-  "`name` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL," +
-  "`serviceLBA` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL," +
-  "`serviceLBC` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL," +
-  "`serviceLBM` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL," +
-  "`Latitude` decimal(15,13) DEFAULT NULL," +
-  "`Longitude` decimal(15,13) DEFAULT NULL," +
-  "PRIMARY KEY (`InspireID`)" +
+  "`slots` int(11) DEFAULT NULL," +
+  "`estate_id` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL," +
+  "`shelter_id` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL," +
+  "`address` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL," +
+  "`city` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL," +
+  "`municipality` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL," +
+  "`position_lat` decimal(15,13) DEFAULT NULL," +
+  "`position_long` decimal(15,13) DEFAULT NULL," +
+  "PRIMARY KEY (`shelter_id`), " +
+  "KEY `position_long` (`position_long`)," +
+  "KEY `position_lat` (`position_lat`)" +
 ") ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;"
 )
