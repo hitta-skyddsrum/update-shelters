01e6c1274ac6fc2909f01d938984b16b8117666c
kontrollanten <alexander@width.se>
2019-02-18 23:36:05 +0100

    rest-api: fix shelters routes

--- rest-api/routes/shelters.go
+++ rest-api/routes/shelters.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+  "database/sql"
   "fmt"
   "github.com/gorilla/mux"
   "net/http"
@@ -17,37 +18,75 @@ type Shelter struct {
   slots             int
 }
 
-func (a *App) GetSheltersByBBox (w http.ResponseWriter, r *http.Request) {
-  fmt.Println("Request started")
-  params := mux.Vars(r)
+var ShelterFields = "address, city, municipality, position_lat, position_long, shelter_id, slots"
 
-  if params == nil {
-    panic(fmt.Errorf("bbox-parametern är obligatorisk"))
-  }
+func getShelterFromRow(row interface{ Scan(...interface{}) (error) }) (Shelter, error) {
+  s := Shelter{}
+  err := row.Scan(&s.address, &s.city, &s.municipality, &s.position_lat, &s.position_long, &s.shelter_id, &s.slots)
 
-  bbox := strings.Split(params["bbox"], ",")
-  if len(bbox) != 4 {
-    panic(fmt.Errorf("bbox-parametern har för få värden"))
-  }
+  return s, err
+}
 
-  rows, err := a.DB.Query("SELECT address, city, municipality, position_lat, position_long, shelter_id, slots FROM `shelters` WHERE position_long > `?` AND position_lat > `?` AND position_long < `?` AND position_lat < `?`", bbox[0], bbox[1], bbox[2], bbox[3])
-  if err != nil {
-    fmt.Printf("Selecting shelters failed: %s\n", err)
-    panic(err)
-  }
-  defer rows.Close()
-
+func getSheltersFromRows(rows *sql.Rows) ([]Shelter, error) {
   shelters := make([]Shelter, 0)
   for rows.Next() {
-    s := Shelter{}
-    if err := rows.Scan(&s.address, &s.city, &s.municipality, &s.position_lat, &s.position_long, &s.shelter_id, &s.slots); err != nil {
-      panic(err)
+    s, err := getShelterFromRow(rows)
+    if err != nil {
+      return nil, err
     }
 
     shelters = append(shelters, s)
   }
 
-  fmt.Println("Finished")
+  return shelters, nil
+}
+
+func (a *App) GetSheltersByBBox (w http.ResponseWriter, r *http.Request) error {
+  params := mux.Vars(r)
+
+  if params == nil {
+    return fmt.Errorf("bbox-parametern är obligatorisk")
+  }
+
+  bbox := strings.Split(params["bbox"], ",")
+  if len(bbox) != 4 {
+    return fmt.Errorf("bbox-parametern har för få värden")
+  }
+
+  rows, err := a.DB.Query("SELECT " + ShelterFields + " FROM `shelters` WHERE position_long > `?` AND position_lat > `?` AND position_long < `?` AND position_lat < `?`", bbox[0], bbox[1], bbox[2], bbox[3])
+  if err != nil {
+    return fmt.Errorf("Selecting shelters failed: %s\n", err)
+  }
+  defer rows.Close()
+
+  shelters, err := getSheltersFromRows(rows)
+  if err != nil {
+    return err
+  }
 
   a.RespondWithJson(w, http.StatusOK, shelters)
+
+  return nil
+}
+
+func (a *App) GetShelterById (w http.ResponseWriter, r *http.Request) error {
+  params := mux.Vars(r)
+  if params == nil {
+    return fmt.Errorf("id krävs som parameter.")
+  }
+
+  row := a.DB.QueryRow("SELECT " + ShelterFields + " FROM `shelters` WHERE id = ?", params["id"])
+
+  shelter, err := getShelterFromRow(row)
+  if err == sql.ErrNoRows {
+    a.RespondWithJson(w, http.StatusNotFound, nil)
+    return nil
+  }
+  if err != nil {
+    return err
+  }
+
+  a.RespondWithJson(w, http.StatusOK, shelter)
+
+  return nil
 }
--- rest-api/routes/shelters_test.go
+++ rest-api/routes/shelters_test.go
@@ -46,7 +46,7 @@ func TestGetSheltersByBBoxSuccess (t *testing.T) {
   rows := sqlmock.NewRows([]string{"address", "city", "municipality", "position_lat", "position_long", "shelter_id", "slots"}).
   AddRow(shelter.address, shelter.city, shelter.municipality, shelter.position_lat, shelter.position_long, shelter.shelter_id, shelter.slots)
 
-  sm.ExpectQuery("SELECT address, city, municipality, position_lat, position_long, shelter_id, slots FROM `shelters`").WithArgs(x1, y1, x2, y2).WillReturnRows(rows)
+  sm.ExpectQuery("SELECT " + ShelterFields + " FROM `shelters`").WithArgs(x1, y1, x2, y2).WillReturnRows(rows)
 
   a := App{
     DB: db,
@@ -54,12 +54,9 @@ func TestGetSheltersByBBoxSuccess (t *testing.T) {
   shelters := make([]Shelter, 0)
   shelters = append(shelters, shelter)
   a.On("RespondWithJson", res, http.StatusOK, shelters)
-  // a.AssertCalled(t, "RespondWithJson", res, http.StatusOK, mock.AnythingOfType("[]Shelter"))
-  a.AssertNumberOfCalls(t, "RespondWithJson", 1)
   a.GetSheltersByBBox(res, req)
   a.AssertExpectations(t)
 
-  return
   if err := sm.ExpectationsWereMet(); err != nil {
     t.Errorf("Tests failed: %s", err)
   }
@@ -73,21 +70,13 @@ func TestGetSheltersByBBoxFailureWithoutParams (t *testing.T) {
   req, _ := http.NewRequest("GET", "/shelters", nil)
   res := httptest.NewRecorder()
 
-  defer func() {
-    r := recover()
-
-    if r == nil {
-      t.Errorf("Tests failed: Expected a panic.")
-    }
-
-    err := fmt.Sprintf("%s", r)
-    if m, _ := regexp.MatchString("bbox-parametern är obligatorisk", err); m == false {
-      t.Errorf("Tests failed: Expected a panic with accurate message, got: %s", r)
-    }
-  }()
 
   a := App{}
-  a.GetSheltersByBBox(res, req)
+  err := a.GetSheltersByBBox(res, req)
+
+  if m, _ := regexp.MatchString("bbox-parametern är obligatorisk", fmt.Sprintf("%s", err)); m == false {
+    t.Errorf("Tests failed: Expected an error with accurate message, got: %s", err)
+  }
 }
 
 func TestGetSheltersByBBoxFailureWithBadBBoxParams (t *testing.T) {
@@ -95,19 +84,91 @@ func TestGetSheltersByBBoxFailureWithBadBBoxParams (t *testing.T) {
   res := httptest.NewRecorder()
   req = mux.SetURLVars(req, map[string]string{"bbox": "1"})
 
-  defer func() {
-    r := recover()
+  a := App{}
+  err := a.GetSheltersByBBox(res, req)
 
-    if r == nil {
-      t.Errorf("Tests failed: Expected a panic.")
-    }
+  if m, _ := regexp.MatchString("för få värden", fmt.Sprintf("%s", err)); m == false {
+    t.Errorf("Tests failed: Expected an error with accurate message, got: %s", err)
+  }
+}
 
-    err := fmt.Sprintf("%s", r)
-    if m, _ := regexp.MatchString("för få värden", err); m == false {
-      t.Errorf("Tests failed: Expected panic with accurate message, got: %s", r)
-    }
-  }()
+func TestGetSingleShelterSuccess (t *testing.T) {
+  id := "3223"
+  req, _ := http.NewRequest("GET", fmt.Sprintf("/shelters/%s", id), nil)
+  req = mux.SetURLVars(req, map[string]string{"id": id })
+  res := httptest.NewRecorder()
+  db, sm, err := sqlmock.New()
+  if err != nil {
+    t.Fatalf("Create db mock failed: %s", err)
+  }
+  defer db.Close()
+
+  shelter := Shelter{
+    address: "Sätra solfångare ",
+    city: "Skärholmen",
+    municipality: "Stockholm",
+    position_lat: "16.205",
+    position_long: "59.4234",
+    shelter_id: id,
+    slots: 91,
+  }
+  rows := sqlmock.NewRows([]string{"address", "city", "municipality", "position_lat", "position_long", "shelter_id", "slots"}).
+  AddRow(shelter.address, shelter.city, shelter.municipality, shelter.position_lat, shelter.position_long, shelter.shelter_id, shelter.slots)
+
+  sm.ExpectQuery("SELECT " + ShelterFields + " FROM `shelters` WHERE id = ?").WithArgs(id).WillReturnRows(rows)
+
+  a := App{
+    DB: db,
+  }
+  shelters := make([]Shelter, 0)
+  shelters = append(shelters, shelter)
+  a.On("RespondWithJson", res, http.StatusOK, shelter)
+  a.GetShelterById(res, req)
+  a.AssertExpectations(t)
+
+  if err := sm.ExpectationsWereMet(); err != nil {
+    t.Errorf("Tests failed: %s", err)
+  }
+
+  if res.Code != http.StatusOK {
+    t.Errorf("Expected to retrieve status OK. Got: %d", res.Code)
+  }
+}
+
+func TestGetSingleShelterNotFound (t *testing.T) {
+  req, _ := http.NewRequest("GET", "/shelters/unknown", nil)
+  id := "144"
+  req = mux.SetURLVars(req, map[string]string{"id": id })
+  res := httptest.NewRecorder()
+  db, sm, err := sqlmock.New()
+  if err != nil {
+    t.Fatalf("Create db mock failed: %s", err)
+  }
+  defer db.Close()
+
+  sm.ExpectQuery("SELECT " + ShelterFields + " FROM `shelters` WHERE id = ?").WithArgs(id).WillReturnError(sql.ErrNoRows)
+
+  a := App{
+    DB: db,
+  }
+  a.On("RespondWithJson", res, http.StatusNotFound, nil)
+
+  err = a.GetShelterById(res, req)
+  if err != nil {
+    t.Fatalf("Failed: %s", err)
+  }
+
+  a.AssertExpectations(t)
+}
+
+func TestGetSingleShelterWithoutId (t *testing.T) {
+  req, _ := http.NewRequest("GET", "/shelters/1", nil)
+  res := httptest.NewRecorder()
 
   a := App{}
-  a.GetSheltersByBBox(res, req)
+  err := a.GetShelterById(res, req)
+
+  if m, _ := regexp.MatchString("id krävs", fmt.Sprintf("%s", err)); m == false {
+    t.Errorf("Tests failed: Expected an error with accurate message, got: %s", err)
+  }
 }
